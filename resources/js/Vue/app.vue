b684fe0508216b8edbd7721ea492b2c52cd0d199
Karl Murray <karlm89@me.com>
2021-03-12 08:30:45 -0600

    Installed Vue / Tailwind - Added Component files

---
+++ resources/js/Vue/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <h1>Hello!</h1>
+      <add-item-form />
+      <list-view />
+  </div>
+</template>
+
+<script>
+
+import addItemForm from './Components/addItemForm.vue'
+import listView from './Components/listView.vue'
+
+  export default {
+    components: {
+      addItemForm,
+      listView,
+    }
+  }
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-require('./bootstrap');
+require("./bootstrap");
+
+import Vue from "vue";
+import App from "./Vue/app.vue";
+
+const app = new Vue({
+    el: "#app",
+    components: { App }
+});
